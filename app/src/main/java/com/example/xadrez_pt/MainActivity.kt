b73573f6ced729a7ba90a09e181bed39ed43e597
AndreGuilhermeCorrea <de_correa@msn.com>
2024-02-09 10:16:22 -0300

    Tabuleiro

--- app/src/main/java/com/example/xadrez_pt/MainActivity.kt
+++ app/src/main/java/com/example/xadrez_pt/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.example.xadrez_pt
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.xadrez_pt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
