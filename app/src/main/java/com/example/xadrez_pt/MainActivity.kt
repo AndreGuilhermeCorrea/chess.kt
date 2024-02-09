5ebaa451e96a4d6b2b3414a2d2b11a888347bc74
AndreGuilhermeCorrea <de_correa@msn.com>
2024-02-09 12:50:02 -0300

    Update Tabuleiro e implementando arquitetura MVC

--- app/src/main/java/com/example/xadrez_pt/MainActivity.kt
+++ app/src/main/java/com/example/xadrez_pt/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.xadrez_pt
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.xadrez_pt.controller.TabuleiroActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun onPlayGameClick(view: View) {
+        // Inicia a TabuleiroActivity
+        val intent = Intent(this, TabuleiroActivity::class.java)
+        startActivity(intent)
     }
 
 }
