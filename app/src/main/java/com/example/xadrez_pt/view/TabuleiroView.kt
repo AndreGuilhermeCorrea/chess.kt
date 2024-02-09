12e7bf7fa6905c11150320db15ded19799d099bc
AndreGuilhermeCorrea <de_correa@msn.com>
2024-02-09 13:53:11 -0300

    Del gif's

--- app/src/main/java/com/example/xadrez_pt/view/TabuleiroView.kt
+++ app/src/main/java/com/example/xadrez_pt/view/TabuleiroView.kt
@@ -1,11 +1,13 @@
 package com.example.xadrez_pt.view
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.example.xadrez_pt.R
 
 class TabuleiroView @JvmOverloads constructor(
     context: Context,
@@ -14,22 +16,47 @@ class TabuleiroView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private val paint = Paint()
+    private var selecionaLadrilho: Pair<Int, Int>? = null
+    private val tamanhoPeca = 100f
+    private val margemPeca = 10f
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        drawChessboard(canvas)
+        drawTabuleiro(canvas)
+        drawPecas(canvas)
     }
-    private fun drawChessboard(canvas: Canvas) {
-        val squareSize = width / 8
+
+    private fun drawPecas(canvas: Canvas) {
+
+        if (selecionaLadrilho != null) {
+            val col = selecionaLadrilho!!.first
+            val row = selecionaLadrilho!!.second
+
+            val pieceBitmap = BitmapFactory.decodeResource(resources, R.drawable.bb)
+            val left = col * (tamanhoPeca + margemPeca)
+            val top = row * (tamanhoPeca + margemPeca)
+            canvas.drawBitmap(pieceBitmap, left, top, paint)
+        }
+    }
+
+    private fun drawTabuleiro(canvas: Canvas) {
+
+        val ladrilhoDark = Color.DKGRAY
+        val ladrilhoLight = Color.LTGRAY
+        var isLadrilhoDark: Boolean
+
 
         for (row in 0 until 8) {
             for (col in 0 until 8) {
-                val isDarkSquare = (col + row) % 2 == 1
-                val backgroundColor = if (isDarkSquare) Color.DKGRAY else Color.LTGRAY
+                isLadrilhoDark = (col + row) % 2 == 1
+                paint.color = if (isLadrilhoDark) ladrilhoDark else ladrilhoLight
 
-                paint.color = backgroundColor
-                canvas.drawRect(col * squareSize.toFloat(), row * squareSize.toFloat(),
-                    (col + 1) * squareSize.toFloat(), (row + 1) * squareSize.toFloat(), paint)
+                canvas.drawRect(
+                    col * (tamanhoPeca + margemPeca),
+                    row * (tamanhoPeca + margemPeca),
+                    (col + 1) * (tamanhoPeca + margemPeca) - margemPeca,
+                    (row + 1) * (tamanhoPeca + margemPeca) - margemPeca,
+                    paint)
             }
         }
     }
